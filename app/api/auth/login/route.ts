4c502754b3941453b7f18eb363481b2cf8660a75
Yogesh Gadhewal <yogeshgadhewal28@gmail.com>
2026-06-10 22:53:45 +0530

    fix: login session change to 1 day.

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -47,7 +47,7 @@ export async function POST(req: NextRequest) {
     httpOnly: true,
     sameSite: 'lax',
     path: '/',
-    maxAge: 7 * 24 * 60 * 60,
+    maxAge: 1 * 24 * 60 * 60, // 1 day
     secure: req.nextUrl.protocol === 'https:',
   });
 
